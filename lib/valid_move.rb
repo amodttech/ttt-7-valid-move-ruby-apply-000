0c8419f9d29ad02add44cf86cca4b25013fbb89f
Aaron Amodt <aaron@amodttech.com>
2020-09-14 22:22:29 +0000

    Automatically backed up by Learn

---
+++ lib/valid_move.rb
@@ -0,0 +1,16 @@
+def valid_move?(board, index)
+  if 
+    
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  move = nil
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    move = false
+  else
+    move = true
+  end
+end
